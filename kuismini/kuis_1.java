8c2fbf991601a2d49a37b52a41469881f7fe7f6f
erwindosantoss <erwindosantos2@gmail.com>
2025-03-02 00:46:46 +0700

    rename kuis & repair wrong answer

--- kuismini/kuis_1.java
+++ kuismini/kuis_1.java
@@ -2,17 +2,29 @@ package kuismini;
 
 import java.util.Scanner;
 
-public class index {
+public class kuis_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Parkir Masuk Jam : ");
-        double masuk = sc.nextInt();
+        int masuk = sc.nextInt();
 
         System.out.print("Parkir Keluar Jam : ");
-        double keluar = sc.nextInt(); 
+        int keluar = sc.nextInt(); 
 
-        double jumlahJam = keluar - masuk;
+        int jamParkir = keluar - masuk;
+
+        int biayaParkir = 0;
+
+        if(jamParkir == 1){
+            biayaParkir = 3000;
+        }else if (jamParkir > 1) {
+            int temp = 0;
+            for(int i = 1; i < jamParkir; i++){
+                temp += 1500;
+                biayaParkir = 3000 + temp;
+            }
+        }
 
         System.out.print("Main biliard berapa jam : ");
         int jamBiliard = sc.nextInt();
@@ -20,13 +32,13 @@ public class index {
         int temp = 0;
 
         if(jamBiliard < 3){
-            for(double i = 0; i < jamBiliard; i++){
+            for(int i = 0; i < jamBiliard; i++){
                 temp += 60000;
                 total = temp + 0.10;
             }
         }else if((jamBiliard % 3) == 0){
             temp = jamBiliard / 3;
-            double harga = temp * 150000;
+            int harga = temp * 150000;
             total = harga + 0.10;
         }else {
             temp = jamBiliard % 3;
@@ -35,14 +47,12 @@ public class index {
                 sum += 60000;
             }
             int hehe = (jamBiliard / 3) * 150000;
-            total = sum + hehe + 0.10;
-            
+            total = sum + hehe + 0.10;   
         }
-
-        System.out.println(total);
 
         System.out.print("Beli Makanan : ");
         boolean makanan = sc.nextBoolean();
+        double biayaMakan = 0;
         
         if(makanan == true){
             System.out.print(
@@ -53,17 +63,21 @@ public class index {
             int pilihan = sc.nextInt();
             switch (pilihan) {
                 case 1:
-                    double sate = 10000 + 0.5;
+                    biayaMakan = 10000 + 0.5;
                     break;
                 case 2 :
-                    double soto = 5000 + 0.5;
+                    biayaMakan = 5000 + 0.5;
                 default:
+                System.out.println("Eror");
                     break;
             }
         }
-        
+        System.out.println(
+            "Harga Biliar : " + total + "\n" +
+            "Harga Parkir : " + biayaParkir + "\n" +
+            "Harga Makanan : " + biayaMakan
+        );
 
         sc.close();
-
     }
 }
